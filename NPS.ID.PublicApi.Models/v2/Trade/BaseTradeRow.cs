6e52e89373f778873c97b43902b7136e8590854e
Paweł Okrasa <pawel.okrasa@nordpoolgroup.com>
2023-04-12 15:05:34 +0200

    DEV-30397: Add Public Trade V2 POCO

---
+++ NPS.ID.PublicApi.Models/v2/Trade/BaseTradeRow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Nordpool.ID.PublicApi.v1.Base;
+using Nordpool.ID.PublicApi.v1.Trade;
+
+namespace NPS.ID.PublicApi.Models.v2.Trade
+{
+    /// <summary>Class with base trade fields</summary>
+    public class BaseTradeRow : BaseRow
+    {
+        public string TradeId { get; set; }
+
+        public DateTimeOffset TradeTime { get; set; }
+
+        public TradeState? State { get; set; }
+
+        public Currency? Currency { get; set; }
+
+        public long EventSequenceNo { get; set; }
+        
+        public long RevisionNo { get; set; }
+        
+        /// <summary>A medium length display name for the contract.</summary>
+        public string MediumDisplayName { get; set; }
+        
+        public bool? SelfTrade { get; set; }
+    }
+}
