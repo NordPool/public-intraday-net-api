5d68d123f360c566c9d2ea885f4aa1f9e50fdc64
Mateusz Korga <mateusz.korga@nordpoolgroup.com>
2023-04-13 14:39:49 +0200

    DEV-30400: Update public intraday net api with new models

--- NPS.ID.PublicApi.Models/Properties/AssemblyInfo.cs
+++ NPS.ID.PublicApi.Models/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
  #endif
 [assembly: AssemblyCompany("Nord Pool")]
 [assembly: AssemblyProduct("Intraday API")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright ï¿½ 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.49")]
-[assembly: AssemblyFileVersion("1.0.2.49")]
+[assembly: AssemblyVersion("1.0.2.50")]
+[assembly: AssemblyFileVersion("1.0.2.50")]
---
+++ NPS.ID.PublicApi.Models/v1/throttling/ThrottlingLimitMessage.cs
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2017 Nord Pool.
+ *  This library is intended to aid integration with Nord Pool�s Intraday API and comes without any warranty. Users of this library are responsible for separately testing and ensuring that it works according to their own standards.
+ *  Please send feedback to idapi@nordpoolgroup.com.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Nordpool.ID.PublicApi.v1.Throttling
+{
+	/// <summary>The Intraday Platform uses Throttling Limit messages to inform a client about the number of orders, logins and it's limits.</summary>
+	public class ThrottlingLimitMessage
+	{
+		public long Timestamp { get; set; }
+
+		public long OrdersPer10s { get; set; }
+
+		public long OrdersPer1h { get; set; }
+
+		public long OrdersPer1day { get; set; }
+
+		public long ThrottlingOrderLimitPer10s { get; set; }
+
+		public long ThrottlingOrderLimitPer1h { get; set; }
+
+		public long LoginsPer20s { get; set; }
+
+		public long LoginsPer20min { get; set; }
+
+		public long ThrottlingLoginLimitPer20s { get; set; }
+
+		public long ThrottlingLoginLimitPer20min { get; set; }
+
+	}
+}
