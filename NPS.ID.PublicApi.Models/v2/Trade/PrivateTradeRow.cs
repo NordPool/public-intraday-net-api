c41ec23d67fc55d9b4049e181ea91d2656019a9b
Pavel Daineka <pavel.daineka@nordpoolgroup.com>
2023-05-25 10:56:39 +0200

    DEV-30239 redundant field removed

--- NPS.ID.PublicApi.Models/v2/Trade/PrivateTradeRow.cs
+++ NPS.ID.PublicApi.Models/v2/Trade/PrivateTradeRow.cs
@@ -5,8 +5,6 @@ namespace NPS.ID.PublicApi.Models.v2.Trade
 {
     public class PrivateTradeRow : BaseTradeRow
     {
-        public long SourceTradeId { get; set; }
-
         public List<PrivateTradeLeg> Legs { get; set; }
     }
 }
