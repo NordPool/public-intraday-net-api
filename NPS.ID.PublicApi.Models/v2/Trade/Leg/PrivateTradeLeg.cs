50dd16d73898ccfe70392cbd3e0fa76772a53309
Pavel Daineka <pavel.daineka@nordpoolgroup.com>
2023-05-25 10:26:09 +0200

    DEV-30239 PrivateTrade v2

---
+++ NPS.ID.PublicApi.Models/v2/Trade/Leg/PrivateTradeLeg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nordpool.ID.PublicApi.v1.Order;
+
+namespace NPS.ID.PublicApi.Models.v2.Trade.Leg
+{
+    public class PrivateTradeLeg : BaseTradeLeg
+    {
+        public string UserId { get; set; }
+
+        public string PortfolioId { get; set; }
+
+        public string ClientOrderId { get; set; }
+
+        public OrderType OrderType { get; set; }
+
+        public string OrderId { get; set; }
+
+        public DateTimeOffset DeliveryStart { get; set; }
+
+        public DateTimeOffset DeliveryEnd { get; set; }
+    }
+}
---
+++ NPS.ID.PublicApi.Models/v2/Trade/PrivateTradeRow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NPS.ID.PublicApi.Models.v2.Trade.Leg;
+
+namespace NPS.ID.PublicApi.Models.v2.Trade
+{
+    public class PrivateTradeRow : BaseTradeRow
+    {
+        public long SourceTradeId { get; set; }
+
+        public List<PrivateTradeLeg> Legs { get; set; }
+    }
+}
