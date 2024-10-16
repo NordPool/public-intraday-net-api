2d32035dc387d3d22883f837903a67f50315a2e9
Pavel Daineka <pavel.daineka@nordpoolgroup.com>
2024-10-16 11:48:34 +0200

    DEV-37512 contract phase added

---
+++ NPS.ID.PublicApi.Models/v1/trade/TradeContractPhaseEnum.cs
@@ -0,0 +1,9 @@
+namespace NPS.ID.PublicApi.Models.v1.trade
+{
+    public enum TradeContractPhaseEnum
+    {
+        NONE = 0,
+        CONT = 1,
+        AUCT = 2
+    }
+}
--- NPS.ID.PublicApi.Models/v2/Trade/PrivateTradeRow.cs
+++ NPS.ID.PublicApi.Models/v2/Trade/PrivateTradeRow.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using NPS.ID.PublicApi.Models.v1.trade;
 using NPS.ID.PublicApi.Models.v2.Trade.Leg;
 
 namespace NPS.ID.PublicApi.Models.v2.Trade
 {
     public class PrivateTradeRow : BaseTradeRow
     {
+        public TradeContractPhaseEnum ContractPhase { get; set; }
+        
         public List<PrivateTradeLeg> Legs { get; set; }
     }
 }
