bdf65237e7e7f5140e4163f899f67cad49222dbd
mayele <michael.ayele@nordpoolgroup.com>
2025-01-13 12:46:57 +0200

    DEV-38511: Fix copyright text encoding for apostrophes across models

--- NPS.ID.PublicApi.Models/v1/PrivateTradeRow.cs
+++ NPS.ID.PublicApi.Models/v1/PrivateTradeRow.cs
@@ -1,6 +1,6 @@
 /*
  *  Copyright 2017 Nord Pool.
- *  This library is intended to aid integration with Nord Pool�s Intraday API and comes without any warranty. Users of this library are responsible for separately testing and ensuring that it works according to their own standards.
+ *  This library is intended to aid integration with Nord Pool's Intraday API and comes without any warranty. Users of this library are responsible for separately testing and ensuring that it works according to their own standards.
  *  Please send feedback to idapi@nordpoolgroup.com.
  */
 
