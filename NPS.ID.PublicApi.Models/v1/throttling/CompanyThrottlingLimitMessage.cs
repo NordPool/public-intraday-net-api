214985b0300b77ed24a1b23607b21aed6bfae050
Cloud Admin Jaakko Myllymäki <cloudadminjaakko.myllymaki@npspotas.onmicrosoft.com>
2026-02-05 14:42:47 +0200

    DEV-44558: Added company throttling limits

---
+++ NPS.ID.PublicApi.Models/v1/throttling/CompanyThrottlingLimitMessage.cs
@@ -0,0 +1,24 @@
+/*
+ *  Copyright 2026 Nord Pool.
+ *  This library is intended to aid integration with Nord Pool’s Intraday API and comes without any warranty. Users of this library are responsible for separately testing and ensuring that it works according to their own standards.
+ *  Please send feedback to idapi@nordpoolgroup.com.
+ */
+
+using System.Collections.Generic;
+
+namespace Nordpool.ID.PublicApi.v1.Throttling
+{
+	/// <summary>The Intraday Platform uses Throttling Limit messages to inform a client about the number of orders, logins and it's limits.</summary>
+	public class CompanyThrottlingLimitMessage
+	{
+		public long LoginsPer20s { get; set; }
+
+		public long LoginsPer20min { get; set; }
+
+		public long ThrottlingLoginLimitPer20s { get; set; }
+
+		public long ThrottlingLoginLimitPer20min { get; set; }
+
+		public List<MarketOrderLimits> OrderLimits { get; set; }
+	}
+}
